0ce0cba3d4b2961e4b6e702d0c9ee2480605224f
dan <danutbug@gmail.com>
2016-02-15 22:45:44 +0200

    do

--- sensors.c
+++ sensors.c
@@ -1,22 +1,42 @@
 #include <QRTSensors.h>
 #define NUM_WHITELINE_SENSORS 4
+#define NUM_KEYENCE_SENSORS 4
 
-QRTSensorsRC white_line_sensors((unsigned char []) {A0, A1, A2, A3}, NUM_WHITELINE_SENSORS)
+QRTSensorsRC white_line_sensors((unsigned char []) {A0, A1, A2, A3}, NUM_WHITELINE_SENSORS);
 unsigned int white_line_values[NUM_WHITELINE_SENSORS];
+int keyence_values[NUM_KEYENCE_SENSORS];
 
-unsigned int[NUM_WHITELINE_SENSORS] get_whiteline_array()
+void init_keyence_array()
 {
-	return white_line_values;
+	pinMode(KEYENCE_LEFT_PIN, OUTPUT);
+	pinMode(KEYENCE_CENTER_PIN, OUTPUT);
+	pinMode(KEYENCE_RIGHT_PIN, OUTPUT);
 }
 
-void setup() 
+void init_omron_array()
+{
+	pinMode(OMRON_PIN, OUTPUT);
+}
+
+void init_whiteline_array()
 {
 	//Just want to test this calibrate method 
 	for (int i = 0; i < 400; i++)
 		white_line_sensors.calibrate();
 }
 
-void loop()
+int[NUM_KEYENCE_SENSORS] get_keyence_array()
+{
+	return keyence_values;
+}
+
+int get_omron()
+{
+	return digitalRead(OMRON_PIN);
+}
+
+unsigned int[NUM_WHITELINE_SENSORS] get_whiteline_array()
 {
 	white_line_sensors.read(white_line_values);
+	return white_line_values;
 }
--- sensors.h
+++ sensors.h
@@ -1,25 +1,34 @@
 #ifndef SENSOR_INTERFACE
 #define SENSOR_INTERFACE
 
+#define WHITELINE_FRONT_LEFT_PIN	A0
+#define WHITELINE_FRONT_RIGHT_PIN	A1
+#define WHITELINE_REAR_LEFT_PIN		A2
+#define WHITELINE_REAR_RIGHT_PIN	A3
+#define KEYENCE_LEFT_PIN			D9
+#define KEYENCE_CENTER_PIN			D10
+#define KEYENCE_RIGHT_PIN			D11
+#define OMRON_PIN					MISO // Aici cred ca trbeuie dat exact numarul pinului
+
 #define KEYENCE_LEFT 0
 #define KEYENCE_CENTER 1
 #define KEYENCE_RIGHT 2
 
 #define NUM_WHITELINE_SENSORS 4
-#define WHITELINE_FRONT_LEFT A0
-#define WHITELINE_FRONT_RIGHT A1
-#define WHITELINE_REAR_LEFT A2
-#define WHITELINE_REAR_RIGHT A3
+#define WHITELINE_FRONT_LEFT 0
+#define WHITELINE_FRONT_RIGHT 1
+#define WHITELINE_REAR_LEFT 2
+#define WHITELINE_REAR_RIGHT 3
 
 #define WHITELINE_DISTANCE 2 	// 2mm? - schimba aici daca nu e in mm
 
 
-void init_keyence_array(int[3] pins);
-void init_whiteline_aray(int[4] pins);	// daca trebuie si distanta, foloseste definitia din fisierul asta
-void init_omron(int pin);
+void init_keyence_array();
+void init_whiteline_array();	// daca trebuie si distanta, foloseste definitia din fisierul asta
+void init_omron();
 
 int[3] get_keyence_array();	// in cazul in care trebuie trimisi si pinii ca argument, sa schimbi linistit
-unsigned int[4] get_whiteline_array();
+unsigned int[NUM_WHITELIEN_SENSORS] get_whiteline_array();
 int get_omron();
 
 #endif
