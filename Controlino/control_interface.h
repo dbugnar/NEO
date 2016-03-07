c1a132f7a86b91d8d023f78a8cbd8308cc49c499
razvanbretoiu <rarazvan1@yahoo.com>
2016-03-08 00:06:22 +0200

    Update control_interface.h

--- Controlino/control_interface.h
+++ Controlino/control_interface.h
@@ -11,11 +11,27 @@
 
 #define DIR_CONTROL_OFFSET 1
 #define CENTER_CONTROL_OFFSET 20
-#define MAX_SPEED 20
+#define MAX_SPEED 255
 #define DONT_MOVE 0
+//Motors
+#define BUFFER_LENGTH 9
+typedef struct
+{
+	uint8_t packetStart[3];
+	int16_t pMotorR_sds16;
+	int16_t pMotorL_sds16;
+	int16_t yCheck_sds16;
+} motorCommand;
+
+typedef union
+{
+	motorCommand sMotorCommand;
+	uint8_t data[BUFFER_LENGTH];
+} rawPacket;
+
 
 void writeMotors(int a, int b);
 
-
 #endif
+
 
