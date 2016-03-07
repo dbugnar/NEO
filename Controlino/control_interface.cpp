ca5c674c30b805d27525644a60a9db1f0609f9eb
razvanbretoiu <rarazvan1@yahoo.com>
2016-03-08 00:04:57 +0200

    Update control_interface.cpp

--- Controlino/control_interface.cpp
+++ Controlino/control_interface.cpp
@@ -4,4 +4,11 @@
 
 #include "control_interface.h"
 
+void writeMotors(int a, int b)
+{
+	rawPacket sRawPacket_mds = { '#', '#', '#', 0, 0, 0 };
+	sRawPacket_mds.sMotorCommand.pMotorR_sds16 = (int16_t)(m1 * 0.925);
+	sRawPacket_mds.sMotorCommand.pMotorL_sds16 = -m2;
+	Serial1.write(sRawPacket_mds.data, BUFFER_LENGTH);
+}
 
